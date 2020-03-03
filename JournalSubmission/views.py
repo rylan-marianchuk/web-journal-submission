cdcb3dfca7fbfced24569212aaaa79770b3064b9
Rylan Marianchuk <rylan.marianchuk@ucalgary.ca>
2020-03-02 21:26:11 -0700

    Created app and Bootstrap Nav Bar

---
+++ JournalSubmission/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def homePage(request):
+    return render (request, 'JournalSubmission/home.html')
+
+def journalList(request):
+    return HttpResponse('<h1> Journals </h1>')
--- PaperSubmissionSystem/settings.py
+++ PaperSubmissionSystem/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'JournalSubmission.apps.JournalsubmissionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
